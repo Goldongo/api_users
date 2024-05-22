5305695974f3f65d0f2282873700b242e6a7e343
Snah-s <camiloernestosoto@gmail.com>
2024-05-22 00:06:07 -0500

    🚀 Set database

--- api_users/database.py
+++ api_users/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./api_users.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:Ut3c9208@database-1.c4lofrzrpfew.us-east-1.rds.amazonaws.com:3306/api_users"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./api_users.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(

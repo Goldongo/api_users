9c8c56659099ed434f4f6a7e57f2c068f52f0cf2
Snah-s <camiloernestosoto@gmail.com>
2024-05-22 01:31:40 -0500

    🚀 Set db

--- api_users/database.py
+++ api_users/database.py
@@ -1,16 +1,19 @@
-# Honestly I don't know what this does at all. It creates or links a database file I guess.
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:Ut3c9208@database-1.c4lofrzrpfew.us-east-1.rds.amazonaws.com:3306/api_users"
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./api_users.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+DOCKER_MYSQL_HOST = "54.221.47.71"  
+DOCKER_MYSQL_PORT = 3306
+
+
+MYSQL_DATABASE = "users"
+MYSQL_USER = "my_user"
+MYSQL_PASSWORD = "my_password"
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{DOCKER_MYSQL_HOST}:{DOCKER_MYSQL_PORT}/{MYSQL_DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
